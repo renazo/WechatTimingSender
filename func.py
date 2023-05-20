ee7854672af1bb4bc9151450a6bb66a4416894b9
Kiri Renazo <kkmniubi@gmail.com>
2023-05-20 21:26:26 +0800

    Update func.py

--- func.py
+++ func.py
@@ -5,34 +5,6 @@ from datetime import datetime
 import threading
 from pynput import mouse
 
-def validate_date(date_text):
-    try:
-        if date_text != '':
-            datetime.strptime(date_text, '%Y/%m/%d/%H/%M/%S')
-        return True
-    except ValueError:
-        return False
-
-def execute_code():
-    # 在这里添加你想在指定时间执行的代码
-    print("执行代码")
-
-def schedule_execution(input_time):
-    if validate_date(input_time):
-        target_time = datetime.strptime(input_time, '%Y/%m/%d/%H/%M/%S')
-        delay = (target_time - datetime.now()).total_seconds()
-        if delay > 0:
-            threading.Timer(delay, execute_code).start()
-            messagebox.showinfo("成功", "代码将在指定时间执行")
-        else:
-            messagebox.showinfo("错误", "输入的时间已过去，请输入未来的时间")
-    else:
-        messagebox.showinfo("错误", "输入的日期时间格式不正确，请按照 '年/月/日/时/分/秒' 的格式输入")
-
-def submit():
-    input_time = e.get()
-    schedule_execution(input_time)
-
 prompts = '点击获取基本位置后输入你需要执行的时间 然后点击执行来准备执行操作'
 #文本框内容转换
 def PChange(pnum):
@@ -48,8 +20,6 @@ def PChange(pnum):
             prompts = '请点击你的鼠标左键到微信的发送按钮上'
     else:
         prompts = '点击获取基本位置后输入你需要执行的时间 然后点击执行来准备执行操作'
-
-
 
 #获取基本鼠标位置
 sendPx = 0
@@ -68,3 +38,15 @@ def gP():
         sendPx, sendPy = pyautogui.position()
     PChange(100)
     print(inputPx, inputPy, sendPx, sendPy)
+
+
+#验证输入的时间是否正确(格式应为: 年/月/日/时/分/秒(年月日可省略 时分秒若早于当前时间则自动转为明天),输入框变量为date)
+
+
+
+    #按下执行后执行代码
+    def execute_code():
+        #需要执行的内容 在指定时间复制内容到剪切板 粘贴到输入框(inputPx, inputPy)中 然后左键单击发送(sendPx, sendPy)
+        print("执行代码")
+
+
